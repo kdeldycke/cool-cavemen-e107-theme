9ed7bb4e7a2adbf58ef5d5c3b29cb889a01ecb0e
Kevin Deldycke <kevin@deldycke.com>
2005-02-27 12:00:00 +0100

    Update header with a photo of all members of the band. Add a texture to the right column.

--- theme.php
+++ theme.php
@@ -3,7 +3,7 @@
 +---------------------------------------------------------------+
 | Ultimate theme
 |
-| ©Kevin Deldycke 2003-2004
+| ©Kevin Deldycke 2003-2005
 | http://funky-storm.com
 | kevin@funky-storm.com
 |
@@ -14,15 +14,15 @@
 
 // [theme]
 $themename = "Cool Cavemen";
-$themeversion = "1.0";
+$themeversion = "1.1";
 $themeauthor = "Kevin Deldycke";
-$themedate = "22/12/2004";
+$themedate = "27/02/2005";
 $themeinfo = "This theme, originally designed for the Cool Cavemen band website (www.coolcavemen.com) and released under the GNU General Public License (http://gnu.org), is the property of Kevin Deldycke (kevin@coolcavemen.com)";
 
 // [layout]
 $layout = "_default";
 
-$logo = THEME."images/logo.png";
+$logo = THEME."images/coolcavemen.png";
 
 $HEADER = "
   <div id=\"header\">
@@ -32,15 +32,26 @@ $HEADER = "
    </ul>
   </div>
 
-  <div id=\"container\">
+  <div class=\"clearall\"/>
+
+  <div id=\"main\">
    <div id=\"sidebar\">
     <div id=\"boxed\">
+
+     <div id=\"quote\">
+      <span class=\"start\">&laquo;</span>
+      <span class=\"stop\">&raquo;</span>
+      A tale of frustration, hardship, and endless reward.
+      <p class=\"author\">-- Eric Meyer</p>
+     </div>
+
      {MENU=2}
     </div>
     <div>
      {MENU=3}
     </div>
     <div>
+     <p><img src=\"".THEME."images/cool_cavemen_button.png\" alt=\"Link us to your site with this button !\"/></p>
      <p><a href=\"http://www.mozilla-europe.org/fr/products/firefox/\"><img src=\"".THEME."images/firefox.gif\" alt=\"Get Firefox Now !\"/></a></p>
      <p><a href=\"http://www.nosoftwarepatents.com/fr/m/intro/index.html\"><img src=\"".THEME."images/nopatents.png\" alt=\"No Software Patent !\"/></a></p>
      <p><img src=\"".THEME."images/music.gif\" alt=\"Music related website\"/></p>
@@ -60,7 +71,7 @@ $FOOTER = "
 
   <div id=\"footer\">
    {MENU=6}
-   <div class=\"spacer\"></div>
+   <div class=\"clearall\"/>
     <div class=\"row\">
      <div class=\"left\">
       {MENU=7}
@@ -73,7 +84,10 @@ $FOOTER = "
     <a href=\"".THEME."/../../../e107_files/backend/news.xml\"><img src=\"".THEME."images/rss2.gif\" alt=\"RSS 2.0 news feed\"/></a>
     <a href=\"http://validator.w3.org/check/referer\"><img src=\"".THEME."images/xhtml.png\" alt=\"xHTML 1.1 valid !\"/></a>
     <a href=\"http://jigsaw.w3.org/css-validator/check/referer\"><img src=\"".THEME."images/css.gif\" alt=\"CSS valid !\"/></a>
-   <div class=\"spacer\"></div>
+    <br/><a href=\"http://fr.creativecommons.org\"><img src=\"".THEME."images/cc.gif\" alt=\"Audio material under Creative Commons licences\"/></a>
+    <br/>
+    Les créations audios disponibles dans la rubrique <a href=\"http://coolcavemen.com/download.php\">download</a> de ce site sont l'oeuvre des Cool Cavemen (G.Deroubaix, T.Haydock, M.Jankowski, T.Jankowski, V.Pouliquen). Ces compositions sont mises à disposition sous un <a href=\"http://creativecommons.org/licenses/by-nc-nd/2.0/fr/\">contrat Creative Commons by-nc-nd</a>.
+   <div class=\"clearall\"/>
    {MENU=9}
   </div>";
 
